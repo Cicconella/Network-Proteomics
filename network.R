4ab41b43f17ad8057ecd686441467ff048b8113e
Cicconella <acmarc@gmail.com>
2017-03-29 16:41:19 +0200

    Script that plots the network given 2 files

--- network.R
+++ network.R
@@ -65,25 +65,15 @@ b$`Fold Change`[b$`Fold Change`<0] = 0.0001
 
 ##### Select the DEs #####
 
-head(expP)
-head(expT)
-dim(expT)
-
-#which(expP$adj.p<0.05)
-
-
-# Get the genes from the both groups
-allGenes = c(as.character(expP[expP$adj.p<0.05,7]),as.character(expP[expT$adj.p<0.01,7]))
+allGenes = c(as.character(a$Protein),as.character(b$Protein))
 allGenes = unique(allGenes)
 allGenes = as.data.frame(allGenes)
-
-write.table(allGenes$allGenes, "/home/cicconella/Desktop/all01", quote = F, row.names = F, col.names = F)
 
 # Get the String Database IDs 
 allGenes = string_db$map(allGenes, "allGenes", removeUnmappedRows = TRUE)
 links = string_db$get_interactions(allGenes[,2])
 
-### Choose here the types of interactions
+### Choose here the types of interactions, in this case, only interactions from experiments are choosen.
 interactions = unique(c(which(links$experiments!=0)))
 links = links[interactions, c(1,2,10)]
 dim(links)
@@ -91,6 +81,7 @@ head(links)
 summary(links$experiments)
 head(links)
 
+### Select the minimum confidence level, in this case, the minimum is set to 0.400.
 links = links[links$experiments>399,]
 dim(links)
 
@@ -104,6 +95,7 @@ for(i in 1:nrow(links)){
 
 head(links)
 
+# Create graph 
 graphObj = graph_from_data_frame(links, directed=F)
 
 # The first group will be "vermelho" (red in portuguese)
@@ -140,25 +132,25 @@ nomes[is.na(nomes[,3]),3] = 0
 
 head(nomes)
 
-# For each gene, we classify it by "trascripts", "proteins"  and "both" 
+# For each gene, we classify it by "trascripts", "proteins"  and "both". The variable is 
 
-cores = c()
+classGene = c()
 
 for(i in 1:nrow(nomes)){
   d = nomes[i,]
   
   if(d[2]==0){
-    cores = c(cores, "transcripts")
+    classGene = c(classGene, "transcripts")
   }else if(d[3]==0){
-    cores = c(cores, "proteins")
+    classGene = c(classGene, "proteins")
   }else{
-    cores = c(cores, "both")
+    classGene = c(classGene, "both")
   }
 }
 
 head(nomes)
 
-nomes = cbind(nomes, cores)
+nomes = cbind(nomes, classGene)
 
 nomes[1:20,]
 
@@ -174,11 +166,9 @@ order
 head(nomes)
 nomes = nomes[order,]
 head(nomes)
-nomes$cores = as.character(nomes$cores)
+nomes$classGene = as.character(nomes$classGene)
 
-
-
-rm(azul, vermelho, cores, i, order)
+rm(azul, vermelho, classGene, i, order)
 
 ##### GO information #####
 
@@ -191,123 +181,78 @@ head(go)
 
 # Merge all informations
 allGenes = merge(allGenes, annotations, by.x = "STRING_id", by.y = "STRING_id")
-#head(allGenes)
+head(allGenes)
 
 allGenes = merge(allGenes, go, by.x = "term_id", by.y = "term_id")
 head(allGenes)
 
 dim(allGenes)
 
-allGenes$pvalue <- format(allGenes$pvalue, scientific = FALSE)
-allGenes$pvalue_fdr <- format(allGenes$pvalue_fdr, scientific = FALSE)
+#If you desire to save the all the GO information about the genes. 
+#allGenes$pvalue = format(allGenes$pvalue, scientific = FALSE)
+#allGenes$pvalue_fdr = format(allGenes$pvalue_fdr, scientific = FALSE)
+#write.table(allGenes, "info.csv", row.names = F)
 
-#write.table(allGenes, "/home/cicconella/Desktop/gogenesProt05.csv", row.names = F)
-
-
-dim(allGenes[allGenes$pvalue_fdr<0.05,])
-
-lista = (unique(allGenes[allGenes$pvalue_fdr<0.01,10]))
-
-lista
-
-
+#If you want to have a list of all functions with at least one gene highly associated
+#lista = (unique(allGenes[allGenes$pvalue_fdr<0.01,10]))
+#lista
 
 # Change the colors classification by format
-formats = rep("pie", length(as.character(nomes$cores)))
-
+formats = rep("pie", length(as.character(nomes$classGene)))
 
 # Define colors of the vertices, if the fold change is positive, it should be in a scale red, if it's  negative, 
 # it should be in a scale of blue. Vertices with two fold change values will be set as white for now.
 
 head(nomes)
-cores = c()
+classGene = c()
 
 for(i in 1:nrow(nomes)){
   d = nomes[i,3]
   
   if(d > 0.5){
-    cores = c(cores, rgb(1, (1-d)*2, (1-d)*2))
+    classGene = c(classGene, rgb(1, (1-d)*2, (1-d)*2))
   } else if(d < 0.5 && d != 0){
-    cores = c(cores, rgb((d*2), (d*2), 1))
+    classGene = c(classGene, rgb((d*2), (d*2), 1))
   } else if(d == 0.5){
-    cores = c(cores, rgb(1,1,1))
+    classGene = c(classGene, rgb(1,1,1))
   } else if(d == 0){
-    cores = c(cores, rgb(0,0,0))
+    classGene = c(classGene, rgb(0,0,0))
   }
 }
 
 
-colorFinal = cores
-cores = c()
+colorFinal = classGene
+classGene = c()
 
 for(i in 1:nrow(nomes)){
   d = nomes[i,2]
   
   if(d > 0.5){
-    cores = c(cores, rgb(1, (1-d)*2, (1-d)*2))
+    classGene = c(classGene, rgb(1, (1-d)*2, (1-d)*2))
   } else if(d < 0.5 && d != 0){
-    cores = c(cores, rgb((d*2), (d*2), 1))
+    classGene = c(classGene, rgb((d*2), (d*2), 1))
   } else if(d == 0.5){
-    cores = c(cores, rgb(1,1,1))
+    classGene = c(classGene, rgb(1,1,1))
   } else if(d == 0){
-    cores = c(cores, rgb(0,0,0))
+    classGene = c(classGene, rgb(0,0,0))
   }
 }
 
-colorFinal = cbind(colorFinal, cores)
+colorFinal = cbind(colorFinal, classGene)
 
-cores = list()
+classGene = list()
 
 for(i in 1:nrow(colorFinal)){
-  cores[[i]] = colorFinal[i,]
+  classGene[[i]] = colorFinal[i,]
 }
 
-cores
+classGene
 
 head(colorFinal)
 head(nomes)
 
 for(i in 1:nrow(nomes)){
   values <- lapply(1:i, function(x) c(5,5))  
-}
-
-threshold = 0.1
-
-if(threshold==0.1){
-  layoutGraph = layout_with_fr(graphObj)
-  set = as.character(nomes$nomes) 
-  newlayout = layoutGraph
-}else{
-  dim((layoutGraph))
-  length(set)
-  
-  nomes$nomes = as.character(nomes$nomes)
-  
-  newlayout = cbind(set, layoutGraph)
-  
-  newlayout = merge(newlayout, nomes, by.x = "set", by.y = "nomes") 
-  
-  head(newlayout)
-  head(nomes)
-  
-  order = c()
-  
-  for(i in 1:length(V(graphObj)$name)){
-    order = c(order, which(as.character(newlayout$set)==V(graphObj)$name[i]))  
-  }
-  
-  order
-  
-  head(newlayout)
-  
-  newlayout = newlayout[order,]
-  
-  head(newlayout)
-  
-  newlayout = newlayout[,c(2,3)]
-  
-  
-  newlayout = cbind(as.numeric(as.character(newlayout[,1])),as.numeric(as.character(newlayout[,2])))
 }
 
 # Choose biological function here
@@ -330,16 +275,17 @@ aux2 = rep("azure4", length(V(graphObj)$name))
 aux2[aux] = "red"
 
 
-png("/home/cicconella/Desktop/actin.png", 
-    width = 1224, height = 1000)
+### Plot ###
+#Plot will be saved in your current directory with the name of the biological function
+png(paste(bio, ".png", sep=""), width = 1224, height = 1000)
 
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+par(mar=c(5.1, 5, 6, 8.2), xpd=TRUE)
 
 par(fig=c(0, 1, 0, 1), xpd=TRUE)
 #, layout = newlayout
 if (interactive()) {
   plot(graphObj, vertex.shape=formats, vertex.pie=values,
-       vertex.pie.color= cores, vertex.label.dist=0.25, vertex.label.degree=pi/2, vertex.label.cex=0.8, 
+       vertex.pie.color= classGene, vertex.label.dist=0.25, vertex.label.degree=pi/2, vertex.label.cex=0.8, 
        vertex.label.color = aux2, vertex.size=7, main = "Gene Network", edge.color = "darkgrey")
 }
 
@@ -347,103 +293,11 @@ testcol<-color.gradient(c(1,1,0),c(0,1,0),c(0,1,1),nslices=100)
 color.legend(1.3,1,1.5,0.3, c("Up[4+]", "","Zero", "", "Down[4-]"), testcol, gradient="y", cex = 0.55)
 text(1.35, 1.1,"Fold Change", cex=0.7, xpd=T)
 
-par(fig=c(0.6, 1, 0, 0.5), new=TRUE)
-
-slices <- c(50,50)
-lbls <- c("Proteins", "Transcripts")
-pie(slices, labels = lbls, clockwise = T, col = c("white","lightgray"), radius = 0.7)
-
-dev.off()
-
-
-bio = "cytoskeleton organization"
-
-# Identify which genes has the selected function
-head(nomes)
-
-aux = allGenes[allGenes$term_description==bio,3]
-aux = as.character(aux)
-aux = as.character(nomes[nomes$nomes %in% aux,1])
-
-for(i in 1:length(aux)){
-  aux[i] = which(V(graphObj)$name==aux[i])
-}
-
-aux = as.numeric(aux)
-aux2 = rep("azure4", length(V(graphObj)$name))
-aux2[aux] = "red"
-
-png("/home/cicconella/Desktop/cyto.png", 
-    width = 1224, height = 1000)
-
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-
-par(fig=c(0, 1, 0, 1), xpd=TRUE)
-#, layout = newlayout
-if (interactive()) {
-  plot(graphObj, vertex.shape=formats, vertex.pie=values,
-       vertex.pie.color= cores, vertex.label.dist=0.25, vertex.label.degree=pi/2, vertex.label.cex=0.65, 
-       vertex.label.color = aux2, vertex.size=7, main = "Gene Network", edge.color = "darkgrey")
-}
-
-testcol<-color.gradient(c(1,1,0),c(0,1,0),c(0,1,1),nslices=100)
-color.legend(1.3,1,1.5,0.3, c("Up", "","Zero", "", "Down"), testcol, gradient="y", cex = 0.55)
-text(1.35, 1.1,"Fold Change", cex=0.7, xpd=T)
-
-par(fig=c(0.6, 1, 0, 0.5), new=TRUE)
-
-slices <- c(50,50)
-lbls <- c("Proteins", "Transcripts")
-pie(slices, labels = lbls, clockwise = T, col = c("white","lightgray"), radius = 0.2)
-
-dev.off()
-
-
-lista = (unique(allGenes[allGenes$pvalue_fdr<0.01,10]))
-
-
-# Choose biological function here
-
-
-
-# Identify which genes has the selected function
-head(nomes)
-
-aux = allGenes[allGenes$term_description==bio,3]
-aux = as.character(aux)
-aux = as.character(nomes[nomes$nomes %in% aux,1])
-
-for(i in 1:length(aux)){
-  aux[i] = which(V(graphObj)$name==aux[i])
-}
-
-aux = as.numeric(aux)
-aux2 = rep("gray", length(V(graphObj)$name))
-aux2[aux] = "black"
-
-png(paste(folder, "bio/", bio,".png", sep=""), width = 960, height = 760)
-
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-
-par(fig=c(0, 1, 0, 1), xpd=TRUE)
-
-if (interactive()) {
-  plot(graphObj, vertex.shape=formats, vertex.pie=values,
-       vertex.pie.color= cores, vertex.label.dist=0.25, vertex.label.degree=pi/2, vertex.label.cex=0.65, 
-       vertex.label.color = aux2, vertex.size=7, main = "Gene Network", edge.color = "darkgrey", 
-       layout = newlayout)
-}
-
-testcol<-color.gradient(c(1,1,0),c(0,1,0),c(0,1,1),nslices=100)
-color.legend(1.3,1,1.5,0.3, c("Up", "","Zero", "", "Down"), testcol, gradient="y", cex = 0.55)
-text(1.35, 1.1,"Fold Change", cex=0.7, xpd=T)
-
-par(fig=c(0.6, 1, 0, 0.5), new=TRUE)
+par(fig=c(0.7, 1, 0, 0.3), new=TRUE)
 
 slices <- c(50,50)
 lbls <- c("Proteins", "Transcripts")
 pie(slices, labels = lbls, clockwise = T, col = c("white","lightgray"), radius = 0.5)
 
 dev.off()
-}
 
