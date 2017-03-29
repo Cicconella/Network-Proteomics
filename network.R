a0ea35a6f3759aec5567c93f68daa06fcd79e36e
Cicconella <acmarc@gmail.com>
2017-03-29 15:40:23 +0200

    Cleaning script. Files for testing.

--- network.R
+++ network.R
@@ -8,33 +8,24 @@ require(igraph)
 require(plotrix)
 library(STRINGdb)
 
-##### Load expression information ######
+##### Load data ######
 
 ### Insert the path to the folder (it will be use for loading and saving all the files)
-folder= "/media/cicconella/8AA6013CA6012A71/Documents and Settings/Nina/Dropbox/metaanalysis/new/" 
+folder= getwd()
 
 ### Load diferential expression genes from proteomics analysis
-expP = read.table(paste(folder, "General_parameters_1D25.txt", sep = ""), header = T, sep = ",")
-head(expP)
-dim(expP)
+a = read.table("Proteomics", header = T)
+head(a)
 
 ### Load diferential expression genes from transcripts data
-expT = read.table(paste(folder ,"General_parameters_20160302_LundData_transcriptomics_freeze_exons.txt", sep = ""), header = T, sep = ",")
-head(expT)
-dim(expT)
-
-head(expP)
-head(expT)
-
-a = expP[,c(2,7)]
-b = expT[,c(2,7)]
-
-head(a)
+b = read.table("Transcripts", header = T)
 head(b)
 
 colnames(a) = c("Fold Change", "Protein")
 colnames(b) = c("Fold Change", "Protein")
 
+dim(a)
+dim(b)
 
 ##### String Database #####
 
@@ -46,7 +37,7 @@ annotations = string_db$get_annotations()
 head(annotations)
 
 
-# Select the maximum absolute values of fold change, it will be used for normalize the both distributions at the same scale
+#Select the maximum absolute values of fold change, it will be used for normalize the both distributions at the same scale
 #maximo = max(abs(min(as.numeric(a$`Fold Change`))), max(as.numeric(a$`Fold Change`)),
 #             abs(min(as.numeric(b$`Fold Change`))), max(as.numeric(b$`Fold Change`)))
 #maximo = ceiling(maximo)
